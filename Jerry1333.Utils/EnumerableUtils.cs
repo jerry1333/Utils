6d21a8f5208b0190cf8f70fc114598be37024e9c
jerry1333 <poczta@jerry1333.net>
2017-10-30 21:08:58 +0100

    Add new utils
    Number related IsBetween (4 variants)
    Enumerable related IsIn (2 variants)

---
+++ Jerry1333.Utils/EnumerableUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jerry1333.Utils
+{
+    public static partial class Utils
+    {
+        /// <summary>
+        ///     in check if element is in provided list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">the value to check</param>
+        /// <param name="values">List of values to look for</param>
+        /// <returns>returns true if the value is in provided values else false</returns>
+        internal static bool IsIn<T>(this T value, params T[] values)
+        {
+            return values != null && values.Contains(value);
+        }
+
+        /// <summary>
+        ///     in check if element is in provided values
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">the value to check</param>
+        /// <param name="values">values to look for</param>
+        /// <returns>returns true if the value is in provided values else false</returns>
+        internal static bool IsIn<T>(this T value, IEnumerable<T> values)
+        {
+            return values != null && values.Contains(value);
+        }
+    }
+}
